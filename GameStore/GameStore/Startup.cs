762d41c11c2506e8689c0620e5332d93f7c46884
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 14:09:28 +0530

    Update Startup.cs
    Included Area specific Route
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- GameStore/GameStore/Startup.cs
+++ GameStore/GameStore/Startup.cs
@@ -69,8 +69,9 @@ namespace GameStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
