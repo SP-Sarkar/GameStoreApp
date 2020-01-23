4c48204960cd75c27696657b612f0db0a361063c
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 13:42:08 +0530

    Create Category.cs
    Category Entity. (Primary Stage)
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities
+{
+    [Table("category")]
+    public class Category : BaseEntity
+    {
+        [Display(Name = "Game Category")]
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Display(Name = "Category Description")]
+        [StringLength(2000)]
+        [DataType(DataType.Html)]
+        public string Description { get; set; }
+    }
+}
