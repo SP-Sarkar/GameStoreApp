3a8f7b181c4bce1e37924f3aa392a6ebbea67813
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:50:32 +0530

    Migration AddGameTableToDatbaseMigration
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Migrations/20200123111835_AddGameTableToDatbaseMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class AddGameTableToDatbaseMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GuidValue = table.Column<Guid>(nullable: false),
+                    CTime = table.Column<DateTime>(nullable: false),
+                    UTime = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    gameName = table.Column<string>(maxLength: 200, nullable: false),
+                    gameUrl = table.Column<string>(maxLength: 200, nullable: true),
+                    gamePrice = table.Column<decimal>(nullable: false),
+                    gameDesc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_games", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "games");
+        }
+    }
+}
