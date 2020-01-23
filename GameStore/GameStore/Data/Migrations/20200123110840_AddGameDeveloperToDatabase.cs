a3f501186ba80949697d6833de61ec8dc1a359f7
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:40:34 +0530

    Migration AddGameDeveloperToDatabase
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Migrations/20200123110840_AddGameDeveloperToDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class AddGameDeveloperToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gameDev",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GuidValue = table.Column<Guid>(nullable: false),
+                    CTime = table.Column<DateTime>(nullable: false),
+                    UTime = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    gameDevName = table.Column<string>(maxLength: 400, nullable: false),
+                    gameDevUrl = table.Column<string>(maxLength: 400, nullable: true),
+                    gameDevDesc = table.Column<string>(nullable: true),
+                    Logo = table.Column<string>(maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gameDev", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gameDev");
+        }
+    }
+}
