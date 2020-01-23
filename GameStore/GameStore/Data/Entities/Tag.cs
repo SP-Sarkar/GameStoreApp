785ac3a6d72a24855ae010dfb1bb5b2138c99fbd
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:35:38 +0530

    Create Tag.cs
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Entities/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities
+{
+    [Table("tags")]
+    public class Tag : BaseEntity
+    {
+
+        [Display(Name = "Tag Label")]
+        [Required]
+        [StringLength(100)]
+        [Column("tagName")]
+        public string Name { get; set; }
+        
+    }
+}
