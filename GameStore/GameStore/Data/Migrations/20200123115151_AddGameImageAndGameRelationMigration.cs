9bfd3d086349731c3f6f0a54d30b6ce16c906ced
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 17:23:24 +0530

    Migration AddGameImageAndGameRelationMigration
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Migrations/20200123115151_AddGameImageAndGameRelationMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class AddGameImageAndGameRelationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameId",
+                table: "gameImages",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_gameImages_GameId",
+                table: "gameImages",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_gameImages_games_GameId",
+                table: "gameImages",
+                column: "GameId",
+                principalTable: "games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_gameImages_games_GameId",
+                table: "gameImages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_gameImages_GameId",
+                table: "gameImages");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "gameImages");
+        }
+    }
+}
