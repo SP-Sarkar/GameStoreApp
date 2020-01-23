92300e7debf27b793e36044bdbb3a2e19f115b8e
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:44:30 +0530

    Migration AddGameImagesToDatabase
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Migrations/20200123111252_AddGameImagesToDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class AddGameImagesToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gameImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GuidValue = table.Column<Guid>(nullable: false),
+                    CTime = table.Column<DateTime>(nullable: false),
+                    UTime = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    ImageUrl = table.Column<string>(maxLength: 500, nullable: true),
+                    AlternativeText = table.Column<string>(maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gameImages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gameImages");
+        }
+    }
+}
