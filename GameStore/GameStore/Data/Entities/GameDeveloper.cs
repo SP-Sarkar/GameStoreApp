b3a2569bc1e7baefac049f60a33779ef7de6442b
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:39:48 +0530

    Create GameDeveloper.cs
    The Game Deb\veloper companyt information.
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Entities/GameDeveloper.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities
+{
+    [Table("gameDev")]
+    public class GameDeveloper : BaseEntity
+    {
+        [Display(Name = "Developer company Name")]
+        [Required]
+        [StringLength(400),Column("gameDevName")]
+        public string Name { get; set; }
+
+        [Display(Name = "Developer company Website")]
+        [StringLength(400), Column("gameDevUrl")]
+        [RegularExpression(@"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$")]
+        public string WebUrl { get; set; }
+
+        [Display(Name = "Developer company Description")]
+        [Column("gameDevDesc")]
+        [DataType(DataType.Html)]
+        public string Description { get; set; }
+
+        [Display(Name = "Developer company Logo")]
+        [StringLength(500)]
+        public string Logo { get; set; }
+    }
+}
