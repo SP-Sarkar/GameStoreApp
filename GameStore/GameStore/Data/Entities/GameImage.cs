aeaff8578aabed47ca86d898cc65d5ee0b02201c
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:43:50 +0530

    Create GameImage.cs
    It will  include all the screenshot of specific games
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Entities/GameImage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities
+{
+    [Table("gameImages")]
+    public class GameImage : BaseEntity
+    {
+        [Display(Name = "Game Screenshots")]
+        [StringLength(500)]
+        public string ImageUrl { get; set; }
+
+        [Display(Name="Alternative text for image")]
+        [StringLength(1000)]
+        public string AlternativeText { get; set; }
+
+        
+    }
+}
