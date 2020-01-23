8887371d2d4d4c7a41b3991648ff2c44e415c9c2
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 17:34:29 +0530

    Update AppDbContext.cs
    Setting navigation property before migration
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- GameStore/GameStore/Data/AppDbContext.cs
+++ GameStore/GameStore/Data/AppDbContext.cs
@@ -13,10 +13,29 @@ namespace GameStore.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<GameCategory>().HasKey(gc => new { gc.GameId, gc.CategoryId });
+
+            builder.Entity<GameCategory>()
+                .HasOne(gc => gc.Game)
+                .WithMany(g => g.GameCategories)
+                .HasForeignKey(gc => gc.GameId);
+
+            builder.Entity<GameCategory>()
+                .HasOne(gc => gc.Category)
+                .WithMany(c => c.GameCategories)
+                .HasForeignKey(gc => gc.CategoryId);
+
+            base.OnModelCreating(builder);
+        }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<GameDeveloper> GameDevelopers { get; set; }
         public DbSet<GameImage> GameImages { get; set; }
         public DbSet<Game> Games { get; set; }
+        public DbSet<GameCategory> GameCategories { get; set; }
     }
 }
