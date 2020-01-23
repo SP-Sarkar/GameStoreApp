3298e58aa27316f5b65fcb34f435438721fc56dd
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:50:10 +0530

    Create Game.cs
    Added Game entity.
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Entities/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities
+{
+    [Table("games")]
+    public class Game : BaseEntity
+    {
+        [Display(Name = "Game Name")]
+        [Column("gameName")]
+        [Required, StringLength(200)]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$")]
+        public string Name { get; set; }
+
+        [Display(Name = "Website URL")]
+        [Column("gameUrl")]
+        [StringLength(200)]
+        [RegularExpression(@"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$")]
+        public string WebUrl { get; set; }
+
+        [Display(Name = "Game Price")]
+        [Column("gamePrice")]
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Game Description")]
+        [Column("gameDesc")]
+        [DataType(DataType.Html)]
+        public string Description { get; set; }
+
+        
+    }
+}
