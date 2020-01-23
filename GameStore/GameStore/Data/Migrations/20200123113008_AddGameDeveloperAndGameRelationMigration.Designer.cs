974e1810656296ca1012b43d571ac09e1c0db2ee
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 17:01:51 +0530

    Migration AddGameDeveloperAndGameRelationMigration
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- GameStore/GameStore/Data/Migrations/20200123113008_AddGameDeveloperAndGameRelationMigration.Designer.cs
+++ GameStore/GameStore/Data/Migrations/20200123113008_AddGameDeveloperAndGameRelationMigration.Designer.cs
@@ -4,14 +4,16 @@ using GameStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameStore.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200123113008_AddGameDeveloperAndGameRelationMigration")]
+    partial class AddGameDeveloperAndGameRelationMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GameStore/GameStore/Data/Migrations/20200123113008_AddGameDeveloperAndGameRelationMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class AddGameDeveloperAndGameRelationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "gameDeveloper",
+                table: "games",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_gameDeveloper",
+                table: "games",
+                column: "gameDeveloper");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_games_gameDev_gameDeveloper",
+                table: "games",
+                column: "gameDeveloper",
+                principalTable: "gameDev",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_games_gameDev_gameDeveloper",
+                table: "games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_games_gameDeveloper",
+                table: "games");
+
+            migrationBuilder.DropColumn(
+                name: "gameDeveloper",
+                table: "games");
+        }
+    }
+}
