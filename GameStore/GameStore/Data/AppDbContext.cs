b38d4a99197eb0f97c938908ab3576760d5c074c
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 16:40:10 +0530

    Update AppDbContext.cs
    Added GameDeveloper for migration
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

--- GameStore/GameStore/Data/AppDbContext.cs
+++ GameStore/GameStore/Data/AppDbContext.cs
@@ -15,5 +15,6 @@ namespace GameStore.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<GameDeveloper> GameDevelopers { get; set; }
     }
 }
