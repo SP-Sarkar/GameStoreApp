35be832546eab67007294713ed9a2ca3f9abd9f9
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-23 13:41:23 +0530

    Create BaseEntity.cs
    includes all common entity property.
    
    Co-Authored-By: AppXive Technology <appxive@users.noreply.github.com>

---
+++ GameStore/GameStore/Data/Entities/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public Guid GuidValue { get; set; }
+
+        [Display(Name="Added On")]
+        [DataType(DataType.DateTime)]
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true,DataFormatString = "{0:dd/mm/yyyy hh:mm}")]
+        public DateTime CTime { get; set; }
+
+        [Display(Name = "Updated on")]
+        [DataType(DataType.DateTime)]
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy hh:mm}")]
+        public DateTime UTime { get; set; }
+
+        [Display(Name = "Active")]
+        [Required]
+        [Range(0,1)]
+        public bool IsDeleted { get; set; }
+    }
+}
