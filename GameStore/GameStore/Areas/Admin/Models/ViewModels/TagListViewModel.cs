038daa766b2ed33b46cd15ff0cf60ea0a5acef80
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-24 18:32:36 +0530

    Create TagListViewModel.cs
    ViewModel for index Operation.

---
+++ GameStore/GameStore/Areas/Admin/Models/ViewModels/TagListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GameStore.Data.Entities;
+
+namespace GameStore.Areas.Admin.Models.ViewModels
+{
+    public class TagListViewModel
+    {
+        [Display(Name="Tag Name")]
+        public string Name { get; set; }
+
+        [Display(Name="Number of Products in this Tag")]
+        public int NumberOProducts { get; set; }
+
+        public string QueryString { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
