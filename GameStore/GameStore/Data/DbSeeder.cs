3f4b0c30ffe8e7244887e546cf1ea9beb92d142d
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-24 18:34:38 +0530

    DataBase Seeder class.
    Generating data for tag table.

---
+++ GameStore/GameStore/Data/DbSeeder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameStore.Data.Entities;
+
+namespace GameStore.Data
+{
+    public static class DbSeeder
+    {
+        public static void SeedData(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            TagSeed(context);
+        }
+
+        private static void TagSeed(AppDbContext context)
+        {
+            if (context.Tags.Any()) return;
+
+            IList<Tag> tags = new List<Tag>
+            {
+                new Tag()
+                {
+                    CTime = DateTime.Now,
+                    UTime = DateTime.Now,
+                    GuidValue = Guid.NewGuid(),
+                    Name = "Best Seller All time",
+                    IsDeleted = false
+                },
+                new Tag()
+                {
+                    CTime = DateTime.Now,
+                    UTime = DateTime.Now,
+                    GuidValue = Guid.NewGuid(),
+                    Name = "Highest seller of this month",
+                    IsDeleted = false
+                },
+                new Tag()
+                {
+                    CTime = DateTime.Now,
+                    UTime = DateTime.Now,
+                    GuidValue = Guid.NewGuid(),
+                    Name = "Demo",
+                    IsDeleted = false
+                },
+                new Tag()
+                {
+                    CTime = DateTime.Now,
+                    UTime = DateTime.Now,
+                    GuidValue = Guid.NewGuid(),
+                    Name = "Demo Deleted",
+                    IsDeleted = true
+                },
+            };
+
+            context.Tags.AddRange(tags);
+            context.SaveChanges();
+        }
+    }
+}
