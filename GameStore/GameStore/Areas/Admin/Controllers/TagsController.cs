353a997a07e3aab69dab80bc8626d62dc58f1fbc
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-24 18:04:38 +0530

    Create TagsController.cs

---
+++ GameStore/GameStore/Areas/Admin/Controllers/TagsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameStore.Areas.Admin.Models.ViewModels;
+using GameStore.Data;
+using GameStore.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace GameStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TagsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public TagsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+    }
+}
