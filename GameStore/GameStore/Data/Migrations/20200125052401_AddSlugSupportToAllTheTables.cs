0a1d270e26110f1347e68b01c1047b32ac6c9748
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-25 10:58:54 +0530

    Migration AddSlugSupportToAllTheTables

---
+++ GameStore/GameStore/Data/Migrations/20200125052401_AddSlugSupportToAllTheTables.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameStore.Data.Migrations
+{
+    public partial class AddSlugSupportToAllTheTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "tags",
+                maxLength: 220,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "games",
+                maxLength: 220,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "gameImages",
+                maxLength: 220,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "gameDev",
+                maxLength: 220,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "category",
+                maxLength: 220,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "tags");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "games");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "gameImages");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "gameDev");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "category");
+        }
+    }
+}
