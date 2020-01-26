730db6a4d68fc862b9bec0f89fed6ba20cf5d91a
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-26 14:18:23 +0530

    Update CategoryController.cs
    View List of category.

--- GameStore/GameStore/Areas/Admin/Controllers/CategoryController.cs
+++ GameStore/GameStore/Areas/Admin/Controllers/CategoryController.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameStore.Areas.Admin.Models.ViewModels;
+using GameStore.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("category")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index(string active)
+        {
+            CategoryListViewModel model = new CategoryListViewModel();
+
+            // getting the queryString
+            model.QueryString = Request.Query[nameof(active)];
+            model.Categories = null;
+
+            if (model.QueryString != null)
+            {
+                if (string.Compare(model.QueryString, "active", StringComparison.Ordinal) == 0)
+                {
+                    model.Categories = await _db.Categories.Where(t => t.IsDeleted == false).ToListAsync();
+                    model.Title = "Active Categories";
+                }
+                else if (string.Compare(model.QueryString, "notactive", StringComparison.Ordinal) == 0)
+                {
+                    model.Categories = await _db.Categories.Where(t => t.IsDeleted == true).ToListAsync();
+                    model.Title = "All Deleted Categories";
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                model.Categories = await _db.Categories.ToListAsync();
+                model.Title = "All Categories";
+            }
+            return View(model);
         }
     }
 }
