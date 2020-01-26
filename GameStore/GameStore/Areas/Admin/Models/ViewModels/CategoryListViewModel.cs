92ee9bf8101db2d47ee8007ca675180bf7046f82
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-26 14:17:08 +0530

    Create CategoryListViewModel.cs
    category list View model for index view catgory controller.

---
+++ GameStore/GameStore/Areas/Admin/Models/ViewModels/CategoryListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GameStore.Data.Entities;
+
+namespace GameStore.Areas.Admin.Models.ViewModels
+{
+    public class CategoryListViewModel
+    {
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Number of Products ")]
+        public int NumberOProducts { get; set; }
+
+        public string QueryString { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+
+}
