1559dce5065e7fa1641d8042fcd7eab608b44664
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-27 10:41:34 +0530

    Specifying Area and Route.

--- GameStore/GameStore/Areas/Admin/Controllers/ProductController.cs
+++ GameStore/GameStore/Areas/Admin/Controllers/ProductController.cs
@@ -6,8 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("product")]
     public class ProductController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
