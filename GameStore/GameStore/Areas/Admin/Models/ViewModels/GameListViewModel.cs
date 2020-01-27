9930c616027471678522bbc0cf919b320d03b00e
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-27 16:24:55 +0530

    Add GameChangeViewModel
    Add new viewmodel class specifically for create and update method

--- GameStore/GameStore/Areas/Admin/Models/ViewModels/GameListViewModel.cs
+++ GameStore/GameStore/Areas/Admin/Models/ViewModels/GameListViewModel.cs
@@ -21,4 +21,31 @@ namespace GameStore.Areas.Admin.Models.ViewModels
         public string Title { get; set; }
         public IEnumerable<Game> Games { get; set; }
     }
+
+    public class GameChangeViewModel
+    {
+        public string Title { get; set; }
+
+        [Display(Name = "Game Name")]
+        [Required, StringLength(200)]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$")]
+        public string Name { get; set; }
+
+        [Display(Name = "Website URL")]
+        [StringLength(200)]
+        [RegularExpression(@"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$")]
+        public string WebUrl { get; set; }
+
+        [Display(Name = "Game Price")]
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Game Description")]
+        [DataType(DataType.Html)]
+        public string Description { get; set; }
+
+        public string GuidValue { get; set; }
+
+    }
 }
