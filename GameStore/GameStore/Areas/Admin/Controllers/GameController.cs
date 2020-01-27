1c57d0b7a27cad8750bc017c54a1e5356414665c
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-27 16:28:58 +0530

    Add CreateGamePost action method
    insert the user input data.

--- GameStore/GameStore/Areas/Admin/Controllers/GameController.cs
+++ GameStore/GameStore/Areas/Admin/Controllers/GameController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using GameStore.Areas.Admin.Models.ViewModels;
 using GameStore.Data;
+using GameStore.Data.Entities;
+using GameStore.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -63,6 +65,40 @@ namespace GameStore.Areas.Admin.Controllers
                 Title = "Create Neg Game"
             };
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("create-game-post")]
+        public async Task<IActionResult> CreateGamePost(GameChangeViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return View(nameof(CreateGame), model);
+                Game game = new Game
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Price = model.Price,
+                    WebUrl = model.WebUrl,
+                    CTime = DateTime.Now,
+                    UTime = DateTime.Now,
+                    GuidValue = Guid.NewGuid(),
+                    Slug = model.Name.ToSlug(),
+                    TagId = 2,
+                    GameDeveloperId = 2,
+                    IsDeleted = false
+                };
+                _db.Games.Add(game);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Details), new {slug = game.Slug, guid = game.GuidValue.ToString()});
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                ModelState.AddModelError("", "Problem in Game Creation");
+                return View(nameof(CreateGame), model);
+            }
         }
 
     }
