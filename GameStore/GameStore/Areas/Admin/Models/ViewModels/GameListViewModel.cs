2913e239365dc9e88bef501a38b67bcf3ce9f461
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-27 11:15:16 +0530

    Create GameListViewModel.cs
    Add ViewModel for Index Method

---
+++ GameStore/GameStore/Areas/Admin/Models/ViewModels/GameListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GameStore.Data.Entities;
+
+namespace GameStore.Areas.Admin.Models.ViewModels
+{
+    public class GameListViewModel
+    {
+        [Display(Name = "Tag Name")]
+        public string Name { get; set; }
+
+        public string QueryString { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Game> Games { get; set; }
+    }
+}
