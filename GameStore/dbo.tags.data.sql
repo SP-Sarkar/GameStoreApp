24912b7a8c51e655b790d531bb6dbf3ac3c2a014
Subhra Prakash Sarkar <tuhinaryaspbs@gmail.com>
2020-01-27 21:07:40 +0530

    Demo data
    Demo data for Category, gameDeveloper, games and tags data.

---
+++ GameStore/dbo.tags.data.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [dbo].[tags] ON
+INSERT INTO [dbo].[tags] ([Id], [GuidValue], [CTime], [UTime], [IsDeleted], [tagName], [Slug]) VALUES (1, N'ccb207b9-e0ab-46d1-8fbd-c53c13d0f1b5', N'2020-01-25 11:24:08', N'2020-01-25 11:24:08', 0, N'Best Seller All time', N'best-seller-all-time')
+INSERT INTO [dbo].[tags] ([Id], [GuidValue], [CTime], [UTime], [IsDeleted], [tagName], [Slug]) VALUES (2, N'7de007dd-3485-4f70-a328-e634bb7f79d5', N'2020-01-25 11:24:08', N'2020-01-25 11:24:08', 0, N'Highest seller of this month', N'highest-seller-of-this-month')
+INSERT INTO [dbo].[tags] ([Id], [GuidValue], [CTime], [UTime], [IsDeleted], [tagName], [Slug]) VALUES (3, N'91751a09-4fe1-4f65-84c0-616440d33bc8', N'2020-01-25 11:24:08', N'2020-01-27 19:23:41', 1, N'Demo Updated', N'demo-updated')
+INSERT INTO [dbo].[tags] ([Id], [GuidValue], [CTime], [UTime], [IsDeleted], [tagName], [Slug]) VALUES (4, N'4c7d8e9d-1c63-4084-82c5-7d4337ef6158', N'2020-01-25 11:24:08', N'2020-01-25 11:24:08', 1, N'Demo Deleted', N'demo-deleted')
+INSERT INTO [dbo].[tags] ([Id], [GuidValue], [CTime], [UTime], [IsDeleted], [tagName], [Slug]) VALUES (5, N'13f0cb67-8d77-409c-ac1e-6f4b9e1088e4', N'2020-01-27 19:07:43', N'2020-01-27 19:07:43', 0, N'New Arrival', N'new-arrival')
+INSERT INTO [dbo].[tags] ([Id], [GuidValue], [CTime], [UTime], [IsDeleted], [tagName], [Slug]) VALUES (6, N'6ff99ee4-895c-4ff9-9faf-e964e29d181f', N'2020-01-27 20:27:46', N'2020-01-27 20:27:46', 0, N'None', N'none')
+SET IDENTITY_INSERT [dbo].[tags] OFF
