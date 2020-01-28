8f898680f39d4f17834a415c5036f03e76ecfc67
Subhra Prakash Sarkar <tuhinaryaspbs@outlook.com>
2020-01-28 10:50:39 +0530

    create TagDetailsViewModel
    create TagDetailsViewModel for displaying specific tag information and list of games related to this tag.

--- GameStore/GameStore/Areas/Admin/Models/ViewModels/TagListViewModel.cs
+++ GameStore/GameStore/Areas/Admin/Models/ViewModels/TagListViewModel.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -35,6 +35,39 @@ namespace GameStore.Areas.Admin.Models.ViewModels
         [Display(Name = "Active")]
         public bool IsDeleted { get; set; }
 
+        public string GuidValue { get; set; }
+    }
+
+    public class TagDetailsViewModel
+    {
+        public TagDetailsViewModel()
+        {
+            Games = new List<Game>();
+        }
+
+        [Display(Name = "Tag Name")]
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Display(Name = "Created On")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd MMM yyyy HH:mm tt}")]
+        public DateTime CTime { get; set; }
+
+        [Display(Name = "Last Updated")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd MMM yyyy HH:mm tt}")]
+        public DateTime UTime { get; set; }
+
+        [Display(Name = "Games")]
+        [DisplayFormat(NullDisplayText = "No Games Yet")]
+        public ICollection<Game> Games { get; set; }
+
+        [Display(Name = "Active")]
+        public bool IsDeleted { get; set; }
+
+        public string Slug { get; set; }
         public string GuidValue { get; set; }
     }
 }
