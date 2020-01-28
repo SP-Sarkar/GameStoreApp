fdee97d77be2e65d793ee2073d43541f0bf8da36
Subhra Prakash Sarkar <tuhinaryaspbs@outlook.com>
2020-01-28 12:33:41 +0530

    join GameDeveloper with game
    include GameDeveloper entity thorough GameDeveloper Navigation property with game to display Game developer name on game details view.

--- GameStore/GameStore/Areas/Admin/Controllers/GameController.cs
+++ GameStore/GameStore/Areas/Admin/Controllers/GameController.cs
@@ -116,7 +116,7 @@ namespace GameStore.Areas.Admin.Controllers
             {
                 if (guid == null) return NotFound();
                 if (!Guid.TryParse(guid, out Guid parsedGuid)) return NotFound();
-                var gameInDb = await _db.Games.Include(t=>t.Tag).FirstOrDefaultAsync(g => g.GuidValue == parsedGuid);
+                var gameInDb = await _db.Games.Include(t=>t.Tag).Include(gd=>gd.GameDeveloper).FirstOrDefaultAsync(g => g.GuidValue == parsedGuid);
                 if (gameInDb == null) return NotFound();
                 if (!string.Equals(gameInDb.Slug, slug)) return NotFound();
                 return View(gameInDb);
